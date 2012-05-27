6b557c50545bd3c6d7045461f21335cb155a34b3
Murphy <murphyspublic@gmail.com>
2012-05-26 18:31:03 -0600

    Before starting RayTracing Project

--- Game/BalloonGame/Assets/PlayerMovement.js
+++ Game/BalloonGame/Assets/PlayerMovement.js
@@ -7,46 +7,83 @@ var turnSpeed:float = 1;
 
 function Start () {
     charCont = GetComponent(CharacterController);
+    audio.volume = 0;
+    audio.loop = true;
 }
 
 function Update () {
-    animation.CrossFade("rest");
+    // Do some key reactions.
     var axis:float;
-    if(Input.GetAxis("Horizontal")){
-        // Move backwards and forwards
-        axis = Input.GetAxis("Horizontal");
-        charCont.Move(Vector3(0, 0, moveMult * axis * -1));
-        animation.CrossFade("swim_normal");
-
-        // Take care of turning.
-        var cur:float = charCont.transform.eulerAngles.y;
-        var dif:float;
-        if(axis < 0){
-            if(cur != 0){
-                dif = 0 - cur;
-                if (dif < 1 && dif > -1){
-                    charCont.transform.eulerAngles.y = 0;
-                }else{
-                    charCont.transform.eulerAngles.y += dif / 6;
-                }
+    if(Input.GetAxis("Horizontal") || Input.GetAxis("Vertical")){
+        if(Input.GetAxis("Horizontal")){
+            // Play audio
+            fadeInAudio();
+            // Move backwards and forwards
+            var curZ = charCont.transform.position.z;
+            axis = Input.GetAxis("Horizontal");
+            if(curZ < 45 && axis < 0){
+                charCont.Move(Vector3(0, 0, moveMult * axis * -1));
+            }else if(curZ > -45 && axis > 0){
+                charCont.Move(Vector3(0, 0, moveMult * axis * -1));
             }
-        }else if(axis > 0){
-            if(cur != 180){
-                dif = 180 - cur;
-                if (dif < 1 && dif > -1){
-                    charCont.transform.eulerAngles.y = 180;
-                }else{
-                    charCont.transform.eulerAngles.y += dif / 6;
+            animation.CrossFade("swim_normal");
+
+            // Take care of turning.
+            var cur:float = charCont.transform.eulerAngles.y;
+            var dif:float;
+            if(axis < 0){
+                if(cur != 0){
+                    dif = 0 - cur;
+                    if (dif < 1 && dif > -1){
+                        charCont.transform.eulerAngles.y = 0;
+                    }else{
+                        charCont.transform.eulerAngles.y += dif / 6;
+                    }
+                }
+            }else if(axis > 0){
+                if(cur != 180){
+                    dif = 180 - cur;
+                    if (dif < 1 && dif > -1){
+                        charCont.transform.eulerAngles.y = 180;
+                    }else{
+                        charCont.transform.eulerAngles.y += dif / 6;
+                    }
                 }
             }
         }
+        if(Input.GetAxis("Vertical")){
+            fadeInAudio();
+            // Move up and down
+            var curY = charCont.transform.position.y;
+            axis = Input.GetAxis("Vertical");
+            if(curY < 45 && axis > 0){
+                charCont.Move(Vector3(0, moveMult * axis, 0));
+            }else if(curY > -45 && axis < 0){
+                charCont.Move(Vector3(0, moveMult * axis, 0));
+            }
+            animation.CrossFade("swim_normal");
+        }
+    }else{
+        assumeRestState();
     }
 
-    if(Input.GetAxis("Vertical")){
-        // Move up and down
-        axis = Input.GetAxis("Vertical");
-        charCont.Move(Vector3(0, moveMult * axis, 0));
-        animation.CrossFade("swim_normal");
-    }
+}
 
+function assumeRestState(){
+    // Set up rest state
+    animation.CrossFade("rest");
+    fadeOutAudio();
+}
+
+function fadeOutAudio(){
+    var step:float = 0.04;
+    if(audio.volume > 0){
+        audio.volume -= step;
+    }
+}
+function fadeInAudio(){
+    var step:float = 0.04;
+    if(audio.volume < 0.7){
+        audio.volume += step;
+    }
 }
